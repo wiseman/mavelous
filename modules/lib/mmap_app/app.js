e72a992a28aa213cd58c0c4cda385b8031ce8bdb
Pat Hickey <pat@moreproductive.org>
2012-07-15 12:25:21 -0700

    cleanup

--- modules/lib/mmap_app/app.js
+++ modules/lib/mmap_app/app.js
@@ -20,36 +20,8 @@ $(function(){
 
   console.log('appview initialized');
   
-  /* hacky test code: */
-
   setInterval(function() {
-    console.log("tick");
-    /*
-    var curralt = vfrHudModel.get("alt");
-    vfrHudModel.set({alt: (curralt + 1) });
-    */
     mavlinkAPI.update();
-
   }, 1000); 
 
-  /*
-  setTimeout( function () {
-    gpsRawIntModel.set({
-      "fix_type": 3,
-      "lat": 455194392,
-      "lon": -1226321082
-    });
-  }, 3000);
-
-  setTimeout( function () {
-    heartbeatModel.set({
-      "type": 2,
-      "autopilot": 3,
-      "base_mode": 81,
-      "custom_mode": 0,
-      "system_status": 4,
-      "mavlink_version": 3
-    });
-  }, 5000);
-  */
 });
