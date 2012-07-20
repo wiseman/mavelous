c0fd830728db22ae712067672db4e35da7d74dea
Pat Hickey <pat@moreproductive.org>
2012-07-19 22:12:53 -0700

    mmap view: implement onZoomChange and onCenterChange

--- modules/lib/mmap_app/mmapview.js
+++ modules/lib/mmap_app/mmapview.js
@@ -4,8 +4,13 @@ $(function(){
   window.MMapView = Backbone.View.extend({
     initialize: function () {
       var self = this;
+
       this.providerModel = this.options.providerModel;
-      this.providerModel.bind('change', this.onProviderChange, this);
+      this.mapModel      = this.options.mapModel;
+
+      this.providerModel.bind('change:provider', this.onProviderChange, this);
+      this.mapModel.bind('change:zoom', this.onZoomChange, this);
+      this.mapModel.bind('change', this.onCenterChange, this);
 
       /* Setup instance variables: */
       var p = this.providerModel.getProvider();
@@ -14,22 +19,31 @@ $(function(){
        self.map = new MM.Map('map', self.mapLayer, undefined, []);
        self.markerLayer = new MM.MarkerLayer();
        self.map.addLayer(self.markerLayer);
+
+       self.onZoomChange();
+       self.onCenterChange();
       });
-
-    },
-
-    render: function () {
-
 
     },
 
     onProviderChange: function () {
       var self = this;
+      console.log('MMapView.onProviderChange: ' +
+            this.providerModel.get('provider'));
       var p = this.providerModel.getProvider();
       p.constructor(function (provider) {
         self.mapLayer.setProvider(provider);
       });
-    }
+    },
 
+    onZoomChange: function () {
+      var z = this.mapModel.get('zoom');
+      this.map.setZoom(z);
+    },
+
+    onCenterChange: function () {
+      var m = this.mapModel.toJSON();
+      this.map.setCenter({ lat: m.lat, lon: m.lon });
+    }
   });
 });
