e747e6660d390e45d5b380cece35ed3475f83f32
Pat Hickey <pat@moreproductive.org>
2012-07-20 21:28:17 -0700

    mmap model: default zoomed way out, zooms in once we get a fix

--- modules/lib/mmap_app/mmapmodel.js
+++ modules/lib/mmap_app/mmapmodel.js
@@ -4,6 +4,8 @@ $(function(){
   window.MMapModel = Backbone.Model.extend({
     WIDE_ZOOM: 2,
     TIGHT_ZOOM: 16,
+    gotgps: false,
+
     defaults: function () {
       return { lat: 0, lon: 0, zoom: this.WIDE_ZOOM };
     },
@@ -14,6 +16,7 @@ $(function(){
     },
 
     initialize: function () {
+      this.gotgps = false;
       console.log('mmap model initialize');
     },
 
@@ -25,7 +28,14 @@ $(function(){
     onGps: function () {
       var gpslat = this.gps.get('lat');
       var gpslon = this.gps.get('lon');
-      this.set({ lat: gpslat / 1.0e7, lon: gpslon / 1.0e7});
+      var state = { lat: gpslat / 1.0e7, lon: gpslon / 1.0e7 };
+
+      if ( gpslat != 0 && gpslon != 0 && this.gotgps == false ) {
+        this.gotgps = true;
+        state.zoom = this.TIGHT_ZOOM;
+      }
+      this.set(state);
+    },
     }
   });
 });
