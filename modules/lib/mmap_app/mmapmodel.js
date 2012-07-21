fde9167a8be9ca7593fb0e099adb0ed573b8b0e6
Pat Hickey <pat@galois.com>
2012-07-20 19:00:50 -0700

    mmapmodel: if lat, lon == 0 (no gps position), zoom way out.

--- modules/lib/mmap_app/mmapmodel.js
+++ modules/lib/mmap_app/mmapmodel.js
@@ -2,13 +2,21 @@
 $(function(){
 
   window.MMapModel = Backbone.Model.extend({
+    WIDE_ZOOM: 2,
+    TIGHT_ZOOM: 16,
     defaults: function () {
-      return { lat: 45.51947, lon: -122.6341, zoom: 18 };
+      return { lat: 0, lon: 0, zoom: this.WIDE_ZOOM };
     },
 
     validate: function ( attrs ) {
       if ( attrs.zoom > 19 ) return "zoom too high";
       if ( attrs.zoom < 0 )  return "zoom too low";
+      if ( attrs.lat == 0 && attrs.lon == 0) {
+        attrs.zoom = this.WIDE_ZOOM;
+      } else if (this.get('lat') == 0 && this.get('lon') == 0
+                && attrs.zoom != this.WIDE_ZOOM ) {
+        attrs.zoom = this.TIGHT_ZOOM; 
+      }
     },
 
     initialize: function () {
@@ -23,7 +31,6 @@ $(function(){
     onGps: function () {
       var gpslat = this.gps.get('lat');
       var gpslon = this.gps.get('lon');
-      console.log("mmapmodel newlat:" + gpslat.toString());
       this.set({ lat: gpslat / 1.0e7, lon: gpslon / 1.0e7});
     }
   });
