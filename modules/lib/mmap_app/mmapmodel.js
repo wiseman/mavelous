6a848eacde783594392b30c6a209736ba152e946
Pat Hickey <pat@moreproductive.org>
2012-07-20 21:27:07 -0700

    mmap model: fix zoom range validation & slider

--- modules/lib/mmap_app/mmapmodel.js
+++ modules/lib/mmap_app/mmapmodel.js
@@ -9,14 +9,8 @@ $(function(){
     },
 
     validate: function ( attrs ) {
-      if ( attrs.zoom > 19 ) return "zoom too high";
-      if ( attrs.zoom < 0 )  return "zoom too low";
-      if ( attrs.lat == 0 && attrs.lon == 0) {
-        attrs.zoom = this.WIDE_ZOOM;
-      } else if (this.get('lat') == 0 && this.get('lon') == 0
-                && attrs.zoom != this.WIDE_ZOOM ) {
-        attrs.zoom = this.TIGHT_ZOOM; 
-      }
+      if ( attrs.zoom > 18 ) return "zoom too high";
+      if ( attrs.zoom < 1 )  return "zoom too low";
     },
 
     initialize: function () {
