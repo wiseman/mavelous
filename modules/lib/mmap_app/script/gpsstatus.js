6f72818e35e780be7cb2f1a5eb96150adfe414c0
Pat Hickey <pat@moreproductive.org>
2012-08-05 00:09:26 -0700

    gps status: simple popover for detailed info in the spirit of linkstatus
    * as with linkstatus i'm not totally pleased with the popover behavior
    * Progress on issue #4

--- modules/lib/mmap_app/script/gpsstatus.js
+++ modules/lib/mmap_app/script/gpsstatus.js
@@ -34,13 +34,16 @@ $(function(){
     initialize: function() {
       var mavlink = this.options.mavlinkSrc;
       this.$el = this.options.el;
-      this.gps = mavlink.subscribe('GPS_RAW_INT', this.render, this);
-      this.render();
+      this.gps = mavlink.subscribe('GPS_RAW_INT', this.onGPS, this);
+      this.stat = mavlink.subscribe('GPS_STATUS', this.onStatus , this);
+      this.$el.popover({
+        animation: false,
+        placement: "bottom",
+        title: "GPS Details"
+      });
     },
     
-    render: function() {
-      var gps = this.gps;
-      var fix_type = gps.get('fix_type');
+    renderFixType: function(fix_type) {
       this.$el.removeClass('btn-success btn-danger btn-warning btn-inverse');
       if (fix_type >= 3) {
         /* 3D Fix */
@@ -60,6 +63,24 @@ $(function(){
         this.$el.html('GPS: None');
       }
       return this;
+    },
+
+    onGPS: function () {
+      var fix_type = this.gps.get('fix_type');
+      this.renderFixType(fix_type);
+      this.updatePopoverData();
+    },
+
+    onStatus: function () {
+      this.updatePopoverData();
+    },
+
+    updatePopoverData: function () {
+      var visible = this.stat.get('satellites_visible').toString();
+      var lat = (this.gps.get('lat') / 10e6).toFixed(7);
+      var lon = (this.gps.get('lon') / 10e6).toFixed(7);
+      this.$el.attr('data-content', 'Satellites: ' + visible +
+          "<br /> Coordinates: " + lat + ", " + lon  );
     }
 
   });
