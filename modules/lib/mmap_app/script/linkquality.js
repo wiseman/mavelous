be1b75d366c3d0e2ca9287573e0c347255e394cc
Pat Hickey <pat@moreproductive.org>
2012-08-04 23:24:47 -0700

    link quality: measure over latest 10 checks, rather than since server started

--- modules/lib/mmap_app/script/linkquality.js
+++ modules/lib/mmap_app/script/linkquality.js
@@ -9,8 +9,23 @@ $(function(){
       this.link = mavlink.subscribe('META_LINKQUALITY', this.onChange, this);
     },
 
+    history: [],
+    period: 10,
+    current: 0,
+
     onChange: function () {
-      console.log(this.link.toJSON());
+      var latest = this.link.toJSON();
+      var compare = this.history[this.current];
+      this.history[this.current] = latest;
+      this.current = (this.current + 1) % this.period;
+      var delta = { master_in: latest.master_in - compare.master_in
+                  , master_out: latest.master_out - compare.master_out
+                  , mav_loss: latest.mav_loss - compare.mav_loss
+                  , period = this.period };
+      this.render(delta);
+    },
+
+    render: function ( linkstats ) {
     }
   });
 
