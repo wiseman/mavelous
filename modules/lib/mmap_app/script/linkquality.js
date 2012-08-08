d7f49d13652f0801ca4fc10efd0b8cccbcf9e4ce
Pat Hickey <pat@galois.com>
2012-08-08 09:55:02 -0700

    link quality: popover updates live!

--- modules/lib/mmap_app/script/linkquality.js
+++ modules/lib/mmap_app/script/linkquality.js
@@ -9,16 +9,37 @@ $(function(){
       this.link = mavlink.subscribe('META_LINKQUALITY', this.onChange, this);
       this.$el.popover({
         animation: false,
-        placement: "bottom",
+        placement: 'bottom',
         title: "Link Statistics",
+        trigger: 'manual',
+        content: $.proxy(this.renderPopoverContent, this)
       });
+      this.$el.click($.proxy(this.buttonClick, this));
     },
 
     history: [],
     period: 10,
     current: 0,
 
+    popoverShown: false,
+
+    buttonClick: function () {
+      if (this.popoverShown) { 
+        this.$el.popover('hide');
+        this.popoverShown = false;
+      } else {
+        this.$el.popover('show');
+        this.popoverShown = true;
+      }
+    },
+
     onChange: function () {
+      if (this.popoverShown) {
+        this.$el.popover('show');
+      }
+    },
+
+    renderPopoverContent: function () {
       var latest = this.link.toJSON();
       if (!latest) return;
       var compare = this.history[this.current];
@@ -29,26 +50,23 @@ $(function(){
                     , master_out: latest.master_out - compare.master_out
                     , mav_loss: latest.mav_loss - compare.mav_loss
                     , period: this.period };
-        this.render(delta);
+        return this.renderStatString(delta);
       } else { /* History has not filled up yet. */
         compare = this.history[0]
         var delta = { master_in: latest.master_in - compare.master_in
                     , master_out: latest.master_out - compare.master_out
                     , mav_loss: latest.mav_loss - compare.mav_loss
-                    , period: this.current - 1 };
-        this.render(delta);
+                    , period: this.current + 1 };
+        return this.renderStatString(delta);
       }
     },
 
-    statString: function (l) {
+    renderStatString: function (l) {
       return ("In last " + l.period + "s, " +
           l.master_in  + " packets received, " +
           l.master_out + " sent, " + l.mav_loss + " lost");
     },
 
-    render: function ( linkstats ) {
-      this.$el.attr('data-content', this.statString(linkstats));
-    }
   });
 
 });
