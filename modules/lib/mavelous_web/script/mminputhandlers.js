ea6d498eaa5d592a6d5032836063fce9f77d26dc
Pat Hickey <pat@moreproductive.org>
2012-11-24 17:50:23 -0800

    input handlers: delete old, replace with vanilla import
    * pulled input handlers out of latest modestmaps.js (3.3.5)
    * renamed from MM namespace to Mavelous namespace

---
+++ modules/lib/mavelous_web/script/mminputhandlers.js
@@ -0,0 +1,384 @@
+
+$(function(){
+    window.Mavelous = window.Mavelous || {};
+
+    Mavelous.MouseWheelHandler = function() {
+        var handler = {},
+            map,
+            _zoomDiv,
+            prevTime,
+            precise = false;
+
+        function mouseWheel(e) {
+            var delta = 0;
+            prevTime = prevTime || new Date().getTime();
+
+            try {
+                _zoomDiv.scrollTop = 1000;
+                _zoomDiv.dispatchEvent(e);
+                delta = 1000 - _zoomDiv.scrollTop;
+            } catch (error) {
+                delta = e.wheelDelta || (-e.detail * 5);
+            }
+
+            // limit mousewheeling to once every 200ms
+            var timeSince = new Date().getTime() - prevTime;
+            var point = MM.getMousePoint(e, map);
+
+            if (Math.abs(delta) > 0 && (timeSince > 200) && !precise) {
+                map.zoomByAbout(delta > 0 ? 1 : -1, point);
+                prevTime = new Date().getTime();
+            } else if (precise) {
+                map.zoomByAbout(delta * 0.001, point);
+            }
+
+            // Cancel the event so that the page doesn't scroll
+            return MM.cancelEvent(e);
+        }
+
+        handler.init = function(x) {
+            map = x;
+            _zoomDiv = document.body.appendChild(document.createElement('div'));
+            _zoomDiv.style.cssText = 'visibility:hidden;top:0;height:0;width:0;overflow-y:scroll';
+            var innerDiv = _zoomDiv.appendChild(document.createElement('div'));
+            innerDiv.style.height = '2000px';
+            MM.addEvent(map.parent, 'mousewheel', mouseWheel);
+            return handler;
+        };
+
+        handler.precise = function(x) {
+            if (!arguments.length) return precise;
+            precise = x;
+            return handler;
+        };
+
+        handler.remove = function() {
+            MM.removeEvent(map.parent, 'mousewheel', mouseWheel);
+            _zoomDiv.parentNode.removeChild(_zoomDiv);
+        };
+
+        return handler;
+    };
+
+    Mavelous.DoubleClickHandler = function() {
+        var handler = {},
+            map;
+
+        function doubleClick(e) {
+            // Ensure that this handler is attached once.
+            // Get the point on the map that was double-clicked
+            var point = MM.getMousePoint(e, map);
+            // use shift-double-click to zoom out
+            map.zoomByAbout(e.shiftKey ? -1 : 1, point);
+            return MM.cancelEvent(e);
+        }
+
+        handler.init = function(x) {
+            map = x;
+            MM.addEvent(map.parent, 'dblclick', doubleClick);
+            return handler;
+        };
+
+        handler.remove = function() {
+            MM.removeEvent(map.parent, 'dblclick', doubleClick);
+        };
+
+        return handler;
+    };
+
+    // Handle the use of mouse dragging to pan the map.
+    Mavelous.DragHandler = function() {
+        var handler = {},
+            prevMouse,
+            map;
+
+        function mouseDown(e) {
+            if (e.shiftKey || e.button == 2) return;
+            MM.addEvent(document, 'mouseup', mouseUp);
+            MM.addEvent(document, 'mousemove', mouseMove);
+
+            prevMouse = new MM.Point(e.clientX, e.clientY);
+            map.parent.style.cursor = 'move';
+
+            return MM.cancelEvent(e);
+        }
+
+        function mouseUp(e) {
+            MM.removeEvent(document, 'mouseup', mouseUp);
+            MM.removeEvent(document, 'mousemove', mouseMove);
+
+            prevMouse = null;
+            map.parent.style.cursor = '';
+
+            return MM.cancelEvent(e);
+        }
+
+        function mouseMove(e) {
+            if (prevMouse) {
+                map.panBy(
+                    e.clientX - prevMouse.x,
+                    e.clientY - prevMouse.y);
+                prevMouse.x = e.clientX;
+                prevMouse.y = e.clientY;
+                prevMouse.t = +new Date();
+            }
+
+            return MM.cancelEvent(e);
+        }
+
+        handler.init = function(x) {
+            map = x;
+            MM.addEvent(map.parent, 'mousedown', mouseDown);
+            return handler;
+        };
+
+        handler.remove = function() {
+            MM.removeEvent(map.parent, 'mousedown', mouseDown);
+        };
+
+        return handler;
+    };
+
+    Mavelous.MouseHandler = function() {
+        var handler = {},
+            map,
+            handlers;
+
+        handler.init = function(x) {
+            map = x;
+            handlers = [
+                Mavelous.DragHandler().init(map),
+                Mavelous.DoubleClickHandler().init(map),
+                Mavelous.MouseWheelHandler().init(map)
+            ];
+            return handler;
+        };
+
+        handler.remove = function() {
+            for (var i = 0; i < handlers.length; i++) {
+                handlers[i].remove();
+            }
+            return handler;
+        };
+
+        return handler;
+    };
+    Mavelous.TouchHandler = function() {
+        var handler = {},
+            map,
+            maxTapTime = 250,
+            maxTapDistance = 30,
+            maxDoubleTapDelay = 350,
+            locations = {},
+            taps = [],
+            snapToZoom = true,
+            wasPinching = false,
+            lastPinchCenter = null;
+
+        function isTouchable () {
+             var el = document.createElement('div');
+             el.setAttribute('ongesturestart', 'return;');
+             return (typeof el.ongesturestart === 'function');
+        }
+
+        function updateTouches(e) {
+            for (var i = 0; i < e.touches.length; i += 1) {
+                var t = e.touches[i];
+                if (t.identifier in locations) {
+                    var l = locations[t.identifier];
+                    l.x = t.clientX;
+                    l.y = t.clientY;
+                    l.scale = e.scale;
+                }
+                else {
+                    locations[t.identifier] = {
+                        scale: e.scale,
+                        startPos: { x: t.clientX, y: t.clientY },
+                        x: t.clientX,
+                        y: t.clientY,
+                        time: new Date().getTime()
+                    };
+                }
+            }
+        }
+
+        // Test whether touches are from the same source -
+        // whether this is the same touchmove event.
+        function sameTouch (event, touch) {
+            return (event && event.touch) &&
+                (touch.identifier == event.touch.identifier);
+        }
+
+        function touchStart(e) {
+            updateTouches(e);
+        }
+
+        function touchMove(e) {
+            switch (e.touches.length) {
+                case 1:
+                    onPanning(e.touches[0]);
+                    break;
+                case 2:
+                    onPinching(e);
+                    break;
+            }
+            updateTouches(e);
+            return MM.cancelEvent(e);
+        }
+
+        function touchEnd(e) {
+            var now = new Date().getTime();
+            // round zoom if we're done pinching
+            if (e.touches.length === 0 && wasPinching) {
+                onPinched(lastPinchCenter);
+            }
+
+            // Look at each changed touch in turn.
+            for (var i = 0; i < e.changedTouches.length; i += 1) {
+                var t = e.changedTouches[i],
+                    loc = locations[t.identifier];
+                // if we didn't see this one (bug?)
+                // or if it was consumed by pinching already
+                // just skip to the next one
+                if (!loc || loc.wasPinch) {
+                    continue;
+                }
+
+                // we now know we have an event object and a
+                // matching touch that's just ended. Let's see
+                // what kind of event it is based on how long it
+                // lasted and how far it moved.
+                var pos = { x: t.clientX, y: t.clientY },
+                    time = now - loc.time,
+                    travel = MM.Point.distance(pos, loc.startPos);
+                if (travel > maxTapDistance) {
+                    // we will to assume that the drag has been handled separately
+                } else if (time > maxTapTime) {
+                    // close in space, but not in time: a hold
+                    pos.end = now;
+                    pos.duration = time;
+                    onHold(pos);
+                } else {
+                    // close in both time and space: a tap
+                    pos.time = now;
+                    onTap(pos);
+                }
+            }
+
+            // Weird, sometimes an end event doesn't get thrown
+            // for a touch that nevertheless has disappeared.
+            // Still, this will eventually catch those ids:
+
+            var validTouchIds = {};
+            for (var j = 0; j < e.touches.length; j++) {
+                validTouchIds[e.touches[j].identifier] = true;
+            }
+            for (var id in locations) {
+                if (!(id in validTouchIds)) {
+                    delete validTouchIds[id];
+                }
+            }
+
+            return MM.cancelEvent(e);
+        }
+
+        function onHold (hold) {
+            // TODO
+        }
+
+        // Handle a tap event - mainly watch for a doubleTap
+        function onTap(tap) {
+            if (taps.length &&
+                (tap.time - taps[0].time) < maxDoubleTapDelay) {
+                onDoubleTap(tap);
+                taps = [];
+                return;
+            }
+            taps = [tap];
+        }
+
+        // Handle a double tap by zooming in a single zoom level to a
+        // round zoom.
+        function onDoubleTap(tap) {
+            var z = map.getZoom(), // current zoom
+                tz = Math.round(z) + 1, // target zoom
+                dz = tz - z;            // desired delate
+
+            // zoom in to a round number
+            var p = new MM.Point(tap.x, tap.y);
+            map.zoomByAbout(dz, p);
+        }
+
+        // Re-transform the actual map parent's CSS transformation
+        function onPanning (touch) {
+            var pos = { x: touch.clientX, y: touch.clientY },
+                prev = locations[touch.identifier];
+            map.panBy(pos.x - prev.x, pos.y - prev.y);
+        }
+
+        function onPinching(e) {
+            // use the first two touches and their previous positions
+            var t0 = e.touches[0],
+                t1 = e.touches[1],
+                p0 = new MM.Point(t0.clientX, t0.clientY),
+                p1 = new MM.Point(t1.clientX, t1.clientY),
+                l0 = locations[t0.identifier],
+                l1 = locations[t1.identifier];
+
+            // mark these touches so they aren't used as taps/holds
+            l0.wasPinch = true;
+            l1.wasPinch = true;
+
+            // scale about the center of these touches
+            var center = MM.Point.interpolate(p0, p1, 0.5);
+
+            map.zoomByAbout(
+                Math.log(e.scale) / Math.LN2 -
+                Math.log(l0.scale) / Math.LN2,
+                center );
+
+            // pan from the previous center of these touches
+            var prevCenter = MM.Point.interpolate(l0, l1, 0.5);
+
+            map.panBy(center.x - prevCenter.x,
+                           center.y - prevCenter.y);
+            wasPinching = true;
+            lastPinchCenter = center;
+        }
+
+        // When a pinch event ends, round the zoom of the map.
+        function onPinched(p) {
+            // TODO: easing
+            if (snapToZoom) {
+                var z = map.getZoom(), // current zoom
+                    tz =Math.round(z);     // target zoom
+                map.zoomByAbout(tz - z, p);
+            }
+            wasPinching = false;
+        }
+
+        handler.init = function(x) {
+            map = x;
+
+            // Fail early if this isn't a touch device.
+            if (!isTouchable()) return handler;
+
+            MM.addEvent(map.parent, 'touchstart', touchStart);
+            MM.addEvent(map.parent, 'touchmove', touchMove);
+            MM.addEvent(map.parent, 'touchend', touchEnd);
+            return handler;
+        };
+
+        handler.remove = function() {
+            // Fail early if this isn't a touch device.
+            if (!isTouchable()) return handler;
+
+            MM.removeEvent(map.parent, 'touchstart', touchStart);
+            MM.removeEvent(map.parent, 'touchmove', touchMove);
+            MM.removeEvent(map.parent, 'touchend', touchEnd);
+            return handler;
+        };
+
+        return handler;
+    };
+});
