f3c3e18b9977d682896d970641595334db560a16
Pat Hickey <pat@moreproductive.org>
2012-11-24 17:51:33 -0800

    mmapview: strip and simplify
    * get rid of waypoint & guide layer completely
    * windowing model hooked up
    * newly imported input handlers hooked up (but not to windowing model)
    * provider model not hooked up

--- modules/lib/mavelous_web/script/mmapview.js
+++ modules/lib/mavelous_web/script/mmapview.js
@@ -4,79 +4,44 @@ $(function(){
 
   Mavelous.MMapView = Backbone.View.extend({
     initialize: function () {
-      var self = this;
 
-      this.providerModel = this.options.providerModel;
-      this.mapModel      = this.options.mapModel;
-      this.guideModel    = this.options.guideModel;
-
-      this.guideWaypoint = null;
+      this.windowModel = this.options.windowModel;
 
       /* Setup instance variables: */
-//      var p = this.providerModel.getProvider();
- //     p.constructor( function (provider) {
-//        self.mapLayer    = new MM.Layer(provider);
-        var template = 'http://{S}tile.openstreetmap.org/{Z}/{X}/{Y}.png';
-        var subdomains = [ '', 'a.', 'b.', 'c.' ];
-        self.mapLayer = new MM.TemplatedLayer(template, subdomains);
+      var template = 'http://{S}tile.openstreetmap.org/{Z}/{X}/{Y}.png';
+      var subdomains = [ '', 'a.', 'b.', 'c.' ];
+      this.mapLayer = new MM.TemplatedLayer(template, subdomains);
 
-        self.mapHandlers =
-          [ new MMHandlers.MouseWheelHandler( self.mapModel ),
-            new MMHandlers.TouchHandler( self.mapModel, self.guideModel ),
-            new MMHandlers.DoubleClickHandler( self.guideModel )
-          ];
-        //self.map = new MM.Map('map', self.mapLayer, undefined, self.mapHandlers);
-        self.map = new MM.Map('map', self.mapLayer)
-        self.markerLayer = new MM.MarkerLayer();
-        self.map.addLayer(self.markerLayer);
+      var inputhandlers = [ new Mavelous.MouseHandler()
+                          , new Mavelous.TouchHandler() ];
 
-        self.onZoomChange();
-        self.onCenterChange();
+      this.map = new MM.Map('map', this.mapLayer, null, inputhandlers)
 
-//        self.providerModel.bind('change:provider',self.onProviderChange, self);
-        self.mapModel.bind('change:zoom', self.onZoomChange, self);
-        self.mapModel.bind('change', self.onCenterChange, self);
-        self.guideModel.bind('change', self.onGuideWaypointChange, self);
-//      });
+      this.windowModel.bind('change:zoom', this.onZoomChange, this);
+      this.windowModel.bind('change', this.onWindowChange , this);
 
-    },
+      this.onWindowChange();
 
-    onGuideWaypointChange: function () {
-      var loc = this.guideModel.toJSON();
-      if (this.guideWaypoint) {
-        this.guideWaypoint.coord = this.map.locationCoordinate(loc);
-        this.markerLayer.repositionMarker(this.guideWaypoint);
-      } else {
-        this.guideWaypoint = this.createWaypoint(loc, "image/mapmarker.png", 50, 50);
-      }
-    },
-
-    createWaypoint: function (location, imageurl, width, height) {
-      var el = document.createElement('div');
-      el.innerHTML = '<img src="' + imageurl + '"' +
-                     ' width="' + width.toString() + '"' +
-                     ' height="' + height.toString() + '">';
-      el.pixelOffset = { x: -1*width/2 , y: -1*height };
-      this.markerLayer.addMarker(el, location);
-      return el;
-    },
-
-    onProviderChange: function () {
-      var self = this;
-      var p = this.providerModel.getProvider();
-      p.constructor(function (provider) {
-        self.mapLayer.setProvider(provider);
-      });
     },
 
     onZoomChange: function () {
-      var z = this.mapModel.get('zoom');
+      var z = this.windowModel.get('zoom');
+      console.log('mmapview: setting zoom to ' + z.toString());
       this.map.setZoom(z);
     },
 
-    onCenterChange: function () {
-      var m = this.mapModel.toJSON();
+    onWindowChange: function () {
+      var m = this.windowModel.toJSON();
+      console.log('mmapview: setting center to ' + m.lat.toString() + ', ' + m.lon.toString());
       this.map.setCenter({ lat: m.lat, lon: m.lon });
+    },
+
+    onProviderChange: function () {
+      var p = this.providerModel.getProvider();
+      p.constructor(_.bind(function (provider) {
+        this.mapLayer.setProvider(provider);
+      }, this));
     }
+
   });
 });
