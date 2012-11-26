f8be86f211f4d610914eed9329742872a4f396f3
Pat Hickey <pat@moreproductive.org>
2012-11-25 21:40:10 -0800

    factor drone icon out to a provider, add quad and plane pngs from APM planner

--- modules/lib/mavelous_web/script/app.js
+++ modules/lib/mavelous_web/script/app.js
@@ -20,12 +20,14 @@ $(function(){
     text: $('#guidealt-text')
   });
 
+  var leafletDroneIcon = new Mavelous.LeafletDroneIconModel();
   var leafletProviders = new Mavelous.LeafletProviders();
 
   var vehicle = new Mavelous.VehicleLeafletPosition({ mavlinkSrc: mavlinkAPI });
   var map = new Mavelous.LeafletView({ 
     vehicle: vehicle,
-    provider: leafletProviders
+    provider: leafletProviders,
+    vehicleIcon: leafletDroneIcon
   });
 
   var commStatusModel = new Mavelous.CommStatusModel({
--- modules/lib/mavelous_web/script/leafletview.js
+++ modules/lib/mavelous_web/script/leafletview.js
@@ -2,28 +2,11 @@
 $(function(){
   window.Mavelous = window.Mavelous || {};
 
-  var droneMarkerSm = L.Icon.extend({
-    options: {
-      iconUrl: 'image/drone-sm.png',
-      shadowUrl: null,
-      iconAnchor: new L.Point(75, 50),
-      iconSize: new L.Point(150, 100)
-    }
-  });
-
-  var droneMarkerTiny = L.Icon.extend({
-    options: {
-      iconUrl: 'image/drone-tiny.png',
-      shadowUrl: null,
-      iconAnchor: new L.Point(37, 25),
-      iconSize: new L.Point(75, 50)
-    }
-  });
-
   Mavelous.LeafletView = Backbone.View.extend({
     initialize: function () {
 
       this.vehicleModel = this.options.vehicle;
+      this.vehicleIconModel = this.options.vehicleIcon;;
       this.providerModel = this.options.provider;
       this.initializedposition = false;
 
@@ -35,6 +18,8 @@ $(function(){
       });
 
       this.providerModel.bind('change', this.providerChange, this);
+
+      this.vehicleIconModel.bind('change', this.vehicleIconChange, this);
 
       this.vehicleModel.bind('change', this.panMapToVehicle, this);
       this.vehicleModel.bind('change', this.updateVehicleMarker, this);
@@ -62,13 +47,24 @@ $(function(){
       if (!p || !h) return;
       if (this.vehicleMarker === undefined) {
         this.vehicleMarker = new L.Marker(p,
-            { icon: new droneMarkerTiny,
+            { icon: this.vehicleIconModel.getIcon(),
               iconAngle: h });
         this.map.addLayer(this.vehicleMarker);
       } else {
         this.vehicleMarker.setLatLng(p);
         this.vehicleMarker.setIconAngle(h);
       }
+    },
+
+    vehicleIconChange: function () {
+      var p = this.vehicleModel.get('position');
+      var h = this.vehicleModel.get('heading');
+      if (!p || !h) return;
+      this.map.removeLayer(this.vehicleMarker);
+      this.vehicleMarker = new L.Marker(p,
+          { icon: this.vehicleIconModel.getIcon(),
+            iconAngle: h });
+      this.map.addLayer(this.vehicleMarker);
     },
 
     updateVehiclePath: function () {
