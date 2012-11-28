824dbb463b1fd0cea8c04d37d76ab1dc267cc0d4
Pat Hickey <pat@moreproductive.org>
2012-11-27 21:05:24 -0800

    fixups to simple leaflet page

--- modules/lib/mavelous_web/script/leaflet_app.js
+++ modules/lib/mavelous_web/script/leaflet_app.js
@@ -2,8 +2,15 @@
 $(function(){ 
   var mavlinkAPI = new Mavelous.MavlinkAPI({ url: '/mavlink/' });
 
-  var lvehicle = new Mavelous.VehicleLeafletPosition({ mavlinkSrc: mavlinkAPI });
-  var map = new Mavelous.LeafletView({ vehicle: lvehicle });
+  var vehicle = new Mavelous.VehicleLeafletPosition({ mavlinkSrc: mavlinkAPI });
+  var leafletDroneIcon = new Mavelous.LeafletDroneIconModel();
+  var leafletProviders = new Mavelous.LeafletProviders();
+
+  var mapView = new Mavelous.LeafletView({ 
+    vehicle: vehicle,
+    provider: leafletProviders,
+    vehicleIcon: leafletDroneIcon
+  });
 
   setInterval(function() {
     mavlinkAPI.update();
