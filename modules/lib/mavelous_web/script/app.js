34eabaa895de195bb7141709a286da8d6ab8ccfe
John Wiseman <jwiseman@morseproject.com>
2012-12-03 17:13:38 -0800

    Analytics conditional on domain == mavelousdemo.appspot.com.

--- modules/lib/mavelous_web/script/app.js
+++ modules/lib/mavelous_web/script/app.js
@@ -13,17 +13,20 @@ $(function() {
 
   var uri = new goog.Uri(window.location.href);
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36746049-1']);
-  _gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-  })();
+  if (uri.getDomain() === 'mavelousdemo.appspot.com') {
+    console.log('Enabling analytics.');
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36746049-1']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script');
+      ga.type = 'text/javascript';
+      ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(ga, s);
+    })();
+  }
 
   var mavlinkAPI = new Mavelous.MavlinkAPI({ url: '/mavlink/' });
   /* If we see "offline" query parameter in the URL, enable offline
