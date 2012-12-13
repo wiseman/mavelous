f232321086d2b9af1e17b232972c0b183048ae07
John Wiseman <jwiseman@morseproject.com>
2012-12-12 17:12:19 -0800

    Switch to monolithic leaflet-0.5.js file, which saves about 100 javascript file requests.
    Added Kinetic extern file.

---
+++ modules/lib/mavelous_web/externs/kinetic-4.1.2.js
@@ -0,0 +1,106 @@
+var Kinetic = {
+    "Filters": {
+        "Grayscale": function () {},
+        "Brighten": function () {},
+        "Invert": function () {}
+    },
+    "Plugins": function () {},
+    "Global": {
+        "BUFFER_WHITELIST": {
+            "0": {},
+            "1": {},
+            "2": {},
+            "3": {}
+        },
+        "BUFFER_BLACKLIST": {
+            "0": {}
+        },
+        "stages": function () {},
+        "idCounter": {},
+        "tempNodes": function () {},
+        "shapes": function () {},
+        "warn": function () {},
+        "extend": function () {},
+        "_pullNodes": function () {},
+        "_addTempNode": function () {},
+        "_removeTempNode": function () {}
+    },
+    "Type": {
+        "_isElement": function () {},
+        "_isFunction": function () {},
+        "_isObject": function () {},
+        "_isArray": function () {},
+        "_isNumber": function () {},
+        "_isString": function () {},
+        "_hasMethods": function () {},
+        "_getXY": function () {},
+        "_getSize": function () {},
+        "_getPoints": function () {},
+        "_getImage": function () {},
+        "_rgbToHex": function () {},
+        "_hexToRgb": function () {},
+        "_getRandomColorKey": function () {},
+        "_merge": function () {},
+        "_clone": function () {}
+    },
+    "Canvas": function () {},
+    "SceneRenderer": function () {},
+    "HitRenderer": function () {},
+    "Tween": function () {},
+    "Tweens": {
+        "back-ease-in": function () {},
+        "back-ease-out": function () {},
+        "back-ease-in-out": function () {},
+        "elastic-ease-in": function () {},
+        "elastic-ease-out": function () {},
+        "elastic-ease-in-out": function () {},
+        "bounce-ease-out": function () {},
+        "bounce-ease-in": function () {},
+        "bounce-ease-in-out": function () {},
+        "ease-in": function () {},
+        "ease-out": function () {},
+        "ease-in-out": function () {},
+        "strong-ease-in": function () {},
+        "strong-ease-out": function () {},
+        "strong-ease-in-out": function () {},
+        "linear": function () {}
+    },
+    "Transform": function () {},
+    "Collection": function () {},
+    "Animation": function () {},
+    "Node": function () {},
+    "DD": {
+        "anim": {
+            "func": {},
+            "node": {},
+            "id": {},
+            "start": function () {},
+            "stop": function () {}
+        },
+        "moving": {},
+        "offset": {
+            "x": {},
+            "y": {}
+        },
+        "_startDrag": function () {},
+        "_endDrag": function () {}
+    },
+    "Transition": function () {},
+    "Container": function () {},
+    "Stage": function () {},
+    "Layer": function () {},
+    "Group": function () {},
+    "Shape": function () {},
+    "Rect": function () {},
+    "Circle": function () {},
+    "Ellipse": function () {},
+    "Image": function () {},
+    "Polygon": function () {},
+    "Text": function () {},
+    "Line": function () {},
+    "Sprite": function () {},
+    "Star": function () {},
+    "RegularPolygon": function () {},
+    "Path": function () {},
+    "TextPath": function () {}
+};
