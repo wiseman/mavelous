65f652cdb5b8bd20d3154612a84676244b5ea466
John Wiseman <jwiseman@morseproject.com>
2012-12-12 19:08:44 -0800

    Compile script now downloads the compiler if needed.

--- modules/lib/mavelous_web/compile.sh
+++ modules/lib/mavelous_web/compile.sh
@@ -1,26 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 set -o xtrace
-CLOSURE_LIBRARY_DIR=$(dirname "$0")/third_party/closure-library
+set -o errexit
+THIS_DIR=$(dirname "$0")
 
-python $CLOSURE_LIBRARY_DIR/closure/bin/build/closurebuilder.py \
-  --root=${CLOSURE_LIBRARY_DIR}/closure/goog \
-  --root=${CLOSURE_LIBRARY_DIR}/third_party/closure \
-  --root=script/ \
-  --namespace="Mavelous.App" \
-  --output_mode=compiled \
-  --compiler_flags="--compilation_level=SIMPLE_OPTIMIZATIONS" \
-  --compiler_flags="--summary_detail_level=3" \
-  --compiler_flags="--warning_level=VERBOSE" \
-  --compiler_flags="--externs=externs/backbone-0.9.1.js" \
-  --compiler_flags="--externs=externs/jquery-1.7.js" \
-  --compiler_flags="--externs=externs/underscore-1.3.1.js" \
-  --compiler_jar=/home/jwiseman/home/bin/closure-compiler.jar \
-  > script/mavelous-compiled.js
+# Dependencies
+DEPS_ROOT=${THIS_DIR}/.goog
 
+CLOSURE_COMPILER_URL=http://closure-compiler.googlecode.com/files/compiler-latest.zip
+CLOSURE_COMPILER_DIR=${DEPS_ROOT}/compiler
+CLOSURE_LIBRARY_DIR=${THIS_DIR}/third_party/closure-library
+CLOSURE_COMPILER=${CLOSURE_COMPILER_DIR}/compiler.jar
+CLOSURE_BUILDER=${CLOSURE_LIBRARY_DIR}/closure/bin/build/closurebuilder.py
 
-#python $CLOSURE_LIBRARY_DIR/closure/bin/calcdeps.py \
-#  --path $CLOSURE_LIBRARY_DIR \
-#  --input script/pfd.js \
-#  --compiler_jar ~/bin/closure-compiler.jar \
-#  --output_mode compiled > script/pfd-compiled.js
+function ensure_deps() {
+    if [ ! -d "${DEPS_ROOT}" ]; then
+        mkdir -p "${CLOSURE_COMPILER_DIR}"
+        (
+            cd "${CLOSURE_COMPILER_DIR}";
+            curl -O "${CLOSURE_COMPILER_URL}";
+            unzip compiler-latest.zip;
+            rm compiler-latest.zip
+        )
+    fi
+}
 
+ensure_deps
+
+rm -f ${THIS_DIR}/script/mavelous-compiled.js
+
+python ${CLOSURE_BUILDER} \
+    --root=${CLOSURE_LIBRARY_DIR}/closure/goog \
+    --root=${CLOSURE_LIBRARY_DIR}/third_party/closure \
+    --root=script/ \
+    --namespace="Mavelous.App" \
+    --output_mode=compiled \
+    --compiler_flags="--compilation_level=SIMPLE_OPTIMIZATIONS" \
+    --compiler_flags="--summary_detail_level=3" \
+    --compiler_flags="--warning_level=VERBOSE" \
+    --compiler_flags="--externs=${THIS_DIR}/externs/backbone-0.9.1.js" \
+    --compiler_flags="--externs=${THIS_DIR}/externs/jquery-1.7.js" \
+    --compiler_flags="--externs=${THIS_DIR}/externs/kinetic-4.1.2.js" \
+    --compiler_flags="--externs=${THIS_DIR}/externs/leaflet-0.5.js" \
+    --compiler_flags="--externs=${THIS_DIR}/externs/underscore-1.3.1.js" \
+    --compiler_jar=${CLOSURE_COMPILER} \
+    > ${THIS_DIR}/script/mavelous-compiled.js
