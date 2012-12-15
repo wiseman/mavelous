c5f852a0fe8459a362f823bbf5510a74d4ecc114
Greg Osmondson <greg@luckythecat.org>
2012-12-15 10:43:56 -0800

    standalone python script

---
+++ devices/android/standalone_python.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+export EXTERNAL_STORAGE=/mnt/sdcard
+PYTHONPATH=/mnt/sdcard/com.googlecode.pythonforandroid/extras/python
+PYTHONPATH=${PYTHONPATH}:/data/data/com.googlecode.pythonforandroid/files/python/lib/python2.6/lib-dynload
+export PYTHONPATH
+export TEMP=/mnt/storage/com.googlecode.pythonforandroid/extras/python/tmp
+export PYTHON_EGG_CACHE=$TEMP
+export PYTHONHOME=/data/data/com.googlecode.pythonforandroid/files/python
+export LD_LIBRARY_PATH=/data/data/com.googlecode.pythonforandroid/files/python/lib
+/data/data/com.googlecode.pythonforandroid/files/python/bin/python "$@"
+
