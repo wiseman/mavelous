2f53691667f3e069c12e391198f1cba4a22d518a
John Wiseman <jwiseman@morseproject.com>
2012-12-16 17:54:14 -0800

    mavelous-makedeps.sh -> makedeps.sh.

--- modules/lib/mavelous_web/makedeps.sh
+++ modules/lib/mavelous_web/makedeps.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-CLOSURE_LIBRARY_DIR=$(dirname "$0")/third_party/closure-library
+THIS_DIR=$(dirname "$0")
+CLOSURE_LIBRARY_DIR=${THIS_DIR}/third_party/closure-library
 python $CLOSURE_LIBRARY_DIR/closure/bin/calcdeps.py \
   --dep $CLOSURE_LIBRARY_DIR \
-  --path script/mission.js \
-  --path script/missionui.js \
-  --output_mode deps > script/mission-deps.js
+  --path script \
+  --output_mode deps > script/mavelous-deps.js
+
+#python ${CLOSURE_LIBRARY_DIR}/closure/bin/build/closurebuilder.py \
+#  --root=${THIS_DIR}/script \
+#  --root=${THIS_DIR}/third_party/closure-library/closure \
+#  --namespace=mavelous.app \
+#  --output_mode=list \
+#  --output_file=mavelous.js
+
