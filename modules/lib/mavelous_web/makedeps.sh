0e472880b24e99b3e3e88841a3b23d8a99865bfd
John Wiseman <jwiseman@morseproject.com>
2012-12-27 10:51:58 -0800

    Made makedeps.sh script able to be run from anywhere.

--- modules/lib/mavelous_web/makedeps.sh
+++ modules/lib/mavelous_web/makedeps.sh
@@ -3,8 +3,8 @@ THIS_DIR=$(dirname "$0")
 CLOSURE_LIBRARY_DIR=${THIS_DIR}/third_party/closure-library
 python $CLOSURE_LIBRARY_DIR/closure/bin/calcdeps.py \
   --dep $CLOSURE_LIBRARY_DIR \
-  --path script \
-  --output_mode deps > script/mavelous-deps.js
+  --path ${THIS_DIR}/script \
+  --output_mode deps > ${THIS_DIR}/script/mavelous-deps.js
 
 #python ${CLOSURE_LIBRARY_DIR}/closure/bin/build/closurebuilder.py \
 #  --root=${THIS_DIR}/script \
