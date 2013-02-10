0b856d83f726f7e57c7d4e98b6936effc6a4cb71
Pat Hickey <pat@galois.com>
2013-02-09 17:18:36 -0800

    Mavelous.PopoverView moved to its own file

--- modules/lib/mavelous_web/script/popoverbutton.js
+++ modules/lib/mavelous_web/script/popoverbutton.js
@@ -1,7 +1,5 @@
-goog.provide('Mavelous.PopoverView');
 goog.provide('Mavelous.RadioButtonPopoverView');
 goog.provide('Mavelous.SelectedModel');
-
 
 
 /**
@@ -105,71 +103,3 @@ Mavelous.RadioButtonPopoverView.prototype.onButtonClick_ = function(btnindex) {
 };
 
 
-
-/**
- * A popover view.
- * @param {Object} properties View properties.
- * @constructor
- * @extends {Backbone.View}
- */
-Mavelous.PopoverView = function(properties) {
-  goog.base(this, properties);
-};
-goog.inherits(Mavelous.PopoverView, Backbone.View);
-
-
-/**
- * @override
- * @export
- */
-Mavelous.PopoverView.prototype.initialize = function() {
-  _.extend(this, Backbone.Events);
-  this.$el = this.options['button'].$el;
-  var t = this.options['button'].popoverTitle;
-  if (typeof t == 'undefined') t = 'need a popoverTitle in button view!';
-  this.$el.popover({
-    'animation': false,
-    'placement': 'bottom',
-    'title': t,
-    'trigger': 'manual'
-  });
-  this.on('content', this.onContent, this);
-  this.options['button'].registerPopover(this);
-};
-
-
-/**
- * Handles selection change.
- */
-Mavelous.PopoverView.prototype.onSelectedChange = function() {
-  if (this.selectedModel.get('selected')) {
-    this.trigger('selected', true);
-  } else {
-    this.$el.popover('hide');
-  }
-};
-
-
-/**
- * Handles new HTML content.
- * @param {string} c The new HTML.
- */
-Mavelous.PopoverView.prototype.onContent = function(c) {
-  if (this.selectedModel.get('selected')) {
-    this.$el.popover('show', function(pel) {
-      pel.find('.popover-content > *').html(c);
-    });
-  }
-};
-
-
-Mavelous.PopoverView.prototype.element = function(k) {
-  if (this.selectedModel.get('selected')) {
-    this.$el.popover('show', k);
-  }
-};
-
-
-Mavelous.PopoverView.prototype.content = function(k) {
-  this.element(function(e) { k(e.find('.popover-content > *')); });
-};
