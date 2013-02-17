a5e32598cc422903b34f91c619e556f841bbe732
Pat Hickey <pat@moreproductive.org>
2013-02-17 13:25:50 -0800

    flightmode: refactor popover view into FlightModePopoverDelegateView

--- modules/lib/mavelous_web/script/flightmode.js
+++ modules/lib/mavelous_web/script/flightmode.js
@@ -251,7 +251,6 @@ Mavelous.CommandButtonView.prototype.onClick_ = function() {
  * @extends {Backbone.View}
  */
 Mavelous.FlightModeButtonView = function(properties) {
-  this.popoverTitle = 'Flight Commands';
   goog.base(this, properties);
 };
 goog.inherits(Mavelous.FlightModeButtonView, Backbone.View);
@@ -263,15 +262,8 @@ goog.inherits(Mavelous.FlightModeButtonView, Backbone.View);
  */
 Mavelous.FlightModeButtonView.prototype.initialize = function() {
   this.modeModel = this.options['modeModel'];
-  this.commandModel = this.options['commandModel'];
   this.$el = this.options['el'];
   this.modeModel.on('change', this.onChange_, this);
-};
-
-
-Mavelous.FlightModeButtonView.prototype.registerPopover = function(p) {
-  this.popover = p;
-  this.popover.on('selected', this.popoverRender, this);
 };
 
 
@@ -290,7 +282,43 @@ Mavelous.FlightModeButtonView.prototype.onChange_ = function() {
 };
 
 
-Mavelous.FlightModeButtonView.prototype.popoverRender = function() {
+
+/**
+ * Flight mode popover Backbone view.
+ * @param {{el: jQuery, modeModel: Mavelous.FlightModeModel, commandModel:
+ *     Mavelous.CommandLongModel}} properties View properties.
+ * @constructor
+ * @extends {Backbone.View}
+ */
+Mavelous.FlightModePopoverDelegateView = function(properties) {
+  this.popoverTitle = 'Flight Commands';
+  goog.base(this, properties);
+};
+goog.inherits(Mavelous.FlightModePopoverDelegateView , Backbone.View);
+
+/**
+ * @override
+ * @export
+ */
+Mavelous.FlightModePopoverDelegateView.prototype.initialize = function() {
+  this.modeModel = this.options['modeModel'];
+  this.commandModel = this.options['commandModel'];
+};
+
+Mavelous.FlightModePopoverViewDelegate.prototype.popoverCreated = function(el) {
+  this.$el = el;
+  this.setupPopover();
+};
+
+Mavelous.FlightModePopoverViewDelegate.prototype.popoverDestroyed = function() {
+  this.$el = null;
+  this.cleanupSubview('armingButtonView');
+  this.cleanupSubview('loiterButtonView');
+  this.cleanupSubview('rtlButtonView');
+  this.cleanupSubview('landButtonView');
+};
+
+Mavelous.FlightModePopoverDelegateView.prototype.setupPopover = function() {
   var loiter =
       '<a class="btn" id="flightmode-btn-loiter" href="#">Loiter</a>';
   var rtl =
@@ -299,34 +327,36 @@ Mavelous.FlightModeButtonView.prototype.popoverRender = function() {
       '<a class="btn" id="flightmode-btn-land" href="#">Land</a>';
   var arm =
       '<p><a class="btn" id="flightmode-btn-arm" href="#">Arm</a></p>';
-  if (this.popover) {
-    this.popover.content(function(e) {
-      e.html(arm + '<br />' + loiter + rtl + land);
-    });
 
-    this.armingButtonView = new Mavelous.ArmingButtonView({
-      'el': $('#flightmode-btn-arm'),
-      'model': this.modeModel
-    });
+  $el.html(arm + '<br />' + loiter + rtl + land);
 
-    this.loiterButtonView = new Mavelous.CommandButtonView({
-      'el': $('#flightmode-btn-loiter'),
-      'model': this.commandModel,
-      'command': 'NAV_LOITER_UNLIM'
-    });
+  this.armingButtonView = new Mavelous.ArmingButtonView({
+    'el': $('#flightmode-btn-arm'),
+    'model': this.modeModel
+  });
 
-    this.rtlButtonView = new Mavelous.CommandButtonView({
-      'el': $('#flightmode-btn-rtl'),
-      'model': this.commandModel,
-      'command': 'NAV_RETURN_TO_LAUNCH'
-    });
+  this.loiterButtonView = new Mavelous.CommandButtonView({
+    'el': $('#flightmode-btn-loiter'),
+    'model': this.commandModel,
+    'command': 'NAV_LOITER_UNLIM'
+  });
 
-    this.landButtonView = new Mavelous.CommandButtonView({
-      'el': $('#flightmode-btn-land'),
-      'model': this.commandModel,
-      'command': 'NAV_LAND'
-    });
+  this.rtlButtonView = new Mavelous.CommandButtonView({
+    'el': $('#flightmode-btn-rtl'),
+    'model': this.commandModel,
+    'command': 'NAV_RETURN_TO_LAUNCH'
+  });
 
-  }
+  this.landButtonView = new Mavelous.CommandButtonView({
+    'el': $('#flightmode-btn-land'),
+    'model': this.commandModel,
+    'command': 'NAV_LAND'
+  });
 };
 
+Mavelous.FlightModePopoverDelegateView.prototype.cleanupSubview = function (subviewname) {
+  if (this[subviewname]) {
+    this[subviewname].remove();
+    this[subviewname] = null;
+  }
+};
